43a4e571a7d31c8e361bd6f34e5dbecd05c68d6b
Fabricio Migues <fabricio.migues@gmail.com>
2017-09-19 17:44:51 +0200

    redirect after students

--- app/controllers/students/registrations_controller.rb
+++ app/controllers/students/registrations_controller.rb
@@ -1,11 +1,10 @@
-class Teachers::RegistrationsController < Devise::RegistrationsController
+class Students::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
   #   super
-  #   redirect_to students_path
   # end
 
   # POST /resource
@@ -37,7 +36,7 @@ class Teachers::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  protected
+  # protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
@@ -50,6 +49,8 @@ class Teachers::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
+  # end
+
   def after_sign_up_path_for(resource)
     resource
   end
