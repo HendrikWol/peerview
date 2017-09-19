70c9d0ec101307ab8f48245bc2845825b8e2621f
Hendrik Wolleswinkel <hendrikwolleswinkel@me.com>
2017-09-19 12:35:25 +0200

    add devise for student and teachers

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,7 +1,12 @@
 class Teacher < User
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :assignments
 
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  devise_for :teachers, path: 'teachers'
+  devise_for :students, path: 'students'
   devise_for :users
   root to: 'pages#home'
 
