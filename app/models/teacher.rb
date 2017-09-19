b2f175cb708784deab7ea59aa301184e648742a9
Fabricio Migues <fabricio.migues@gmail.com>
2017-09-19 14:57:51 +0200

    teachers half way finished

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,4 +1,8 @@
 class Teacher < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :assignments
 
   validates :first_name, presence: true
