3f8997b65af7802e163902029f7f343d3d2d5abe
Fabricio Migues <fabricio.migues@gmail.com>
2017-09-21 13:27:59 +0200

    Photo added to student profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170921091321) do
+ActiveRecord::Schema.define(version: 20170921104420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 20170921091321) do
     t.datetime "last_sign_in_at"
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
+    t.string "photo"
     t.index ["classroom_id"], name: "index_students_on_classroom_id"
     t.index ["email"], name: "index_students_on_email", unique: true
     t.index ["reset_password_token"], name: "index_students_on_reset_password_token", unique: true
