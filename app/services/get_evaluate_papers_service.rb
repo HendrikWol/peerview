ccb389d0f2d8bd26ebeadbd8fc012d2093c7023f
Hendrik Wolleswinkel <hendrikwolleswinkel@me.com>
2017-09-26 17:15:20 +0200

    Services to display papers to evaluate: DONE'

---
+++ app/services/get_evaluate_papers_service.rb
@@ -0,0 +1,12 @@
+class GetEvaluatePapersService
+
+  def call(current_student)
+      evs = EvaluationProcess.where(evaluator: current_student)
+      result = evs.map do |evaluation|
+        paper = Paper.where(assignment: evaluation.assignment, student: evaluation.evaluated)
+        paper.empty? ? next : paper
+      end
+      result.compact!
+      result.flatten!
+  end
+end
