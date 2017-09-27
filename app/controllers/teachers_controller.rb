374392234835f7c6ba61f70bd2c7824011b794c0
Fabricio Migues <fabricio.migues@gmail.com>
2017-09-27 13:18:53 +0200

    Teacher graph working well

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,25 +1,42 @@
 class TeachersController < ApplicationController
-before_action :set_teacher, only: [:show, :edit, :update, :destroy]
+  before_action :set_teacher, only: [:show, :edit, :update, :destroy]
 
-def index
-  @teachers = Teacher.all
-end
-
-def show
-end
-
-def new
-@teacher = Teacher.new
-end
-
-def create
-@teacher = Teacher.new(teacher_params)
-# @teacher.user_id = current_user
-  if @teacher.save
-  redirect_to teachers_path
-  else
-    render :new
+  def index
+    @teachers = Teacher.all
   end
+
+  def show
+    @teacher_graph_input = []
+    @evaluations = Evaluation.all
+    @assignments = Assignment.all
+    @papers = Paper.all
+    teacher_papers = []
+    @papers.each do |paper|
+    teacher_papers << paper if paper.assignment.topic.teacher.id == current_teacher.id
+    end
+
+    teacher_papers.each do |paper|
+      instance_array = [paper.student.first_name, paper.evaluation.final_grade]
+      @teacher_graph_input << instance_array
+    end
+    @teacher_graph_input
+
+  end
+
+
+  def new
+    @teacher = Teacher.new
+  end
+
+
+  def create
+    @teacher = Teacher.new(teacher_params)
+# @teacher.user_id = current_user
+if @teacher.save
+  redirect_to teachers_path
+else
+  render :new
+end
 end
 
 def edit
@@ -45,7 +62,7 @@ def set_teacher
 end
 
 def teacher_params
-params.require(:teacher).permit(:first_name, :last_name, :courses, :city, :description, :email, :photo, :photo_cache)
+  params.require(:teacher).permit(:first_name, :last_name, :courses, :city, :description, :email, :photo, :photo_cache)
 end
 
 end
